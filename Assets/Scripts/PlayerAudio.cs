b0869e8d5eb06be832991f3d1442c48f00fef810
wiki <wiki0712@hotmail.com.tw>
2017-01-21 20:14:31 +0800

    Add some audios

--- Assets/Scripts/PlayerAudio.cs
+++ Assets/Scripts/PlayerAudio.cs
@@ -5,13 +5,38 @@ using UnityEngine;
 
 public class PlayerAudio : MonoBehaviour {
 
-	public enum Sounds{BABY_HURT, BABY_DEAD, BABY_JUMP};
+	public enum Sounds{
+	                                  ADULT_SCREAM,
+									  ADULT_WALK,
+									  BABY_HURT,  
+									  BABY_LAUGH,
+									  BABY_WALK,
+									  EXPLOSION,
+									  FEMALE_LAUGH,
+									  FEMALE_SCREAM,
+									  HIT,
+	                                  JUMP,
+									  LEVEL_UP,
+									  MONSTER_YELL,
+									  PICK_COIN,
+									  POWER_UP
+	                                  };
 	AudioSource player_audio;  
 	
-	public AudioClip babyHurt;  								// The audio clip to play when the player get hurt.
-    public AudioClip babyDeath;                                 // The audio clip to play when the player dies.
-	public AudioClip babyJump;								
-	
+	public AudioClip adultScream;
+    public AudioClip adultWalk;
+	public AudioClip babyHurt; 
+	public AudioClip babyLaugh;
+    public AudioClip babyWalk;    
+	public AudioClip explosion; 
+	public AudioClip femaleLaugh;
+    public AudioClip femaleScream;
+    public AudioClip hit;
+	public AudioClip jump;
+	public AudioClip levelUp;
+    public AudioClip monsterYell;         		                      
+	public AudioClip pickCoin;
+	public AudioClip powerUp;
 	// Use this for initialization
 	void Awake () {	
 			player_audio = GetComponent <AudioSource> ();
@@ -24,21 +49,56 @@ public class PlayerAudio : MonoBehaviour {
 	
 	public void playSounds(Sounds clip)
 	{
+		bool soundsExist = true;
+		
 		switch(clip){
+			case Sounds.ADULT_SCREAM:
+				player_audio.clip = adultScream;
+				 break;
+			case Sounds.ADULT_WALK:
+				player_audio.clip = adultWalk;
+				 break;
 			case Sounds.BABY_HURT:
 				player_audio.clip = babyHurt;
-				 player_audio.Play ();
 				 break;
-			case Sounds.BABY_DEAD:
-				player_audio.clip = babyDeath;
-				 player_audio.Play ();
+			case Sounds.BABY_LAUGH:
+				player_audio.clip = babyLaugh;
 				 break;
-			case Sounds.BABY_JUMP:
-				player_audio.clip = babyJump;
-				 player_audio.Play ();
+			case Sounds.BABY_WALK:
+				player_audio.clip = babyWalk;
 				 break;
-			
+			case Sounds.EXPLOSION:
+				player_audio.clip = explosion;
+				 break;
+			case Sounds.FEMALE_LAUGH:
+				player_audio.clip = femaleLaugh;
+				 break;
+			case Sounds.FEMALE_SCREAM:
+				player_audio.clip = femaleScream;
+				 break;
+			case Sounds.HIT:
+				player_audio.clip = hit;
+				 break;
+			case Sounds.JUMP:
+				player_audio.clip = jump;
+				 break;
+			case Sounds.LEVEL_UP:
+				player_audio.clip = levelUp;
+				 break;
+			case Sounds.MONSTER_YELL:
+				player_audio.clip = monsterYell;
+				 break;
+			case Sounds.PICK_COIN:
+				player_audio.clip = pickCoin;
+				 break;
+			case Sounds.POWER_UP:
+				player_audio.clip = powerUp;
+				 break;
+			default :
+				soundsExist = false;
+				break;
 		}
 		
+		if(soundsExist) player_audio.Play ();
 	}
 }

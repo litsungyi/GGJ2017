63140aa1c16fa19c9781b8f4929d3026e2d17548
litsungyi <litsungyi@sigono.com>
2017-01-21 10:21:45 +0800

    Add test theme

---
+++ Assets/jumppoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumppoint : MonoBehaviour {
+	//private Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+		//rg = GetComponents<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision col) {
+		if (col.collider.name == "player") {
+			col.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.up*500);
+		}
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	private Rigidbody rg;
+
+	// Use this for initialization
+	private float speed = 5f;
+	void Start () {
+		rg = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward *speed* Time.deltaTime,Space.World);
+		if(Input.GetButtonDown("Jump")) {
+			rg.velocity = new Vector3 (0, 8, 0);
+		}
+
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.name == "door") {
+			transform.position = new Vector3 (-3.87f, 0.69f, -2.78f);
+		}
+	}
+}

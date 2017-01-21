724ed5f843e6afb774f69bdd24fa64639718f124
Paul Ni <nipaulfc@gmail.com>
2017-01-21 17:53:18 +0800

    update uI add start Panel

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    [SerializeField]
+    private Transform player;
+    private int age ;
+    private Text ageTxt;
+	// Use this for initialization
+	void Start () {
+        age = 0;
+        ageTxt = transform.FindChild("ageField/ageTxt").GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(((int)player.localPosition.z/10)> age)
+        {
+            age = ((int)player.localPosition.z / 10);
+            ageTxt.text = age.ToString();
+        }
+	}
+}
---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class start : MonoBehaviour {
+    [SerializeField]
+    private Button startBtn;
+   
+    // Use this for initialization
+    void Start () {
+        startBtn.onClick.AddListener(go);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void go()
+    {
+        this.gameObject.SetActive(false);
+    }
+}

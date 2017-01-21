a7c79cdcafe80facccf738aa9363abcf43741053
litsungyi <litsungyi@sigono.com>
2017-01-21 17:34:39 +0800

    Update prefabs

--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class obstacle : MonoBehaviour {
+public class obstacle : MonoBehaviour, ICollidable
+{
 	private Renderer rend;
 	// Use this for initialization
 	void Start () {
 		rend = GetComponent<Renderer> ();
 	}
-	
-	// Update is called once per frame
-	void OnCollisionEnter (Collision col) {
-		if (col.collider.name == "player") {
-			rend.material.color = Color.red;
-		}
+
+	#region ICollidable implementation
+
+	void ICollidable.OnCollisionEnter(player target)
+	{
+		rend.material.color = Color.red;
 	}
 
+	void ICollidable.OnCollisionExit(player target)
+	{
+	}
+
+	#endregion
 }

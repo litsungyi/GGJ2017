51394d0fdbb60a583244c248f9a4ef9c5fe5f097
litsungyi <litsungyi@sigono.com>
2017-01-22 11:58:39 +0800

    Add player level up

---
+++ Assets/Scripts/PassToPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassToPlayer : MonoBehaviour
+{
+	private player target;
+	private Camera camera;
+
+	private void Awake()
+	{
+		target = GameObject.FindObjectOfType<player>();
+		camera = target.gameObject.GetComponentInChildren<Camera>();
+		var position = camera.transform.localPosition;
+		camera.transform.localPosition = position;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (target != null)
+		{
+			target.OnCollisionEnter(col);
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (target != null)
+		{
+			target.OnTriggerEnter(col);
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (target != null)
+		{
+			target.OnTriggerExit(col);
+		}
+	}
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class player : MonoBehaviour {
 	private bool gameStart;
 	private Rigidbody rg;
+	private BoxCollider boxCollider;
 	private bool waveEnabled;
 	private float originX;
 	private Quaternion originRotation;
+	private int currentModel = 0;
+	[SerializeField] private List<GameObject> models;
 
 	public Transform sparkle;
 	public PlusUpParticleManager plusup;
@@ -20,7 +23,11 @@ public class player : MonoBehaviour {
 	private float speed = 5f;
 	void Awake () {
 		originX = transform.localPosition.x;
-		rg = GetComponent<Rigidbody> ();
+
+		rg = GetComponent<Rigidbody>();
+		boxCollider = GetComponent<BoxCollider>();
+		UpdateModel();
+
 		originRotation = sparkle.transform.localRotation;
 		sparkle.GetComponent<ParticleSystem> ().enableEmission = false;
 		playerAudio = GetComponent<PlayerAudio>();
@@ -102,8 +109,33 @@ public class player : MonoBehaviour {
 		StartCoroutine(stopSparkles());
 	}
 
+	private void UpdateModel()
+	{
+		for (int i = 0; i < models.Count; i++)
+		{
+			if (i == currentModel)
+			{
+				var childCollider = models[i].gameObject.GetComponent<BoxCollider>();
+				boxCollider.center = childCollider.center;
+				boxCollider.size = childCollider.size;
+				models[i].SetActive(true);
+			}
+			else
+			{
+				models[i].SetActive(false);
+			}
+		}
+	}
+
 	public void LevelUp()
 	{
+		++currentModel;
+		if (currentModel >= models.Count)
+		{
+			currentModel = models.Count - 1;
+		}
+
+		UpdateModel();
 		plusup.Enable(true);
 		waveCalc.amplitute += 10f;
 		waveCalc.waveLength *= 0.9f;
@@ -132,7 +164,7 @@ public class player : MonoBehaviour {
 		--health;
 	}
 
-	void OnCollisionEnter(Collision col)
+	public void OnCollisionEnter(Collision col)
 	{
 		var coollidable = col.gameObject.GetComponent<ICollidable>();
 		if (coollidable != null)
@@ -141,7 +173,7 @@ public class player : MonoBehaviour {
 		}
 	}
 
-	void OnTriggerEnter(Collider col)
+	public void OnTriggerEnter(Collider col)
 	{
 		var triggable = col.gameObject.GetComponent<ITriggable>();
 		if (triggable != null)
@@ -149,7 +181,8 @@ public class player : MonoBehaviour {
 			triggable.OnTriggerEnter(this);
 		}
 	}
-    void OnTriggerExit(Collider col)
+
+    public void OnTriggerExit(Collider col)
     {
         var triggable = col.gameObject.GetComponent<ITriggable>();
         if (triggable != null)

ff3e485fd591fc00d16491573acc1bc32596ee98
Paul Ni <nipaulfc@gmail.com>
2017-01-22 08:39:50 +0800

    add background

---
+++ Assets/Scripts/DayNightBackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightBackgroundController : MonoBehaviour {
+    [SerializeField]
+    public Transform player;
+    float currentPlayerZ;
+    // Use this for initialization
+    void Start () {
+        currentPlayerZ = player.localPosition.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //update when move 10
+        if ((player.localPosition.z ) > currentPlayerZ)
+        {
+            transform.Rotate(new Vector3(0, 0, player.localPosition.z-currentPlayerZ));
+            currentPlayerZ = player.localPosition.z ;
+           
+        }
+    }
+}
